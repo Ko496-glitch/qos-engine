355626bc89b8621e634f1cdbf8ae354aaec6a9e4
kartikohlan <kartik7ohlan@gmail.com>
2026-01-17 00:12:37 -0500

    added now() functionality

--- global_buffer.hpp
+++ global_buffer.hpp
@@ -8,10 +8,17 @@
 #include "lib::egresss_buffer.hpp"
 #include "lib::rb_tree.hpp"
 #include "lib::policy.hpp"
-
+#include<chrono>
 
 namespace lib{
   constexpr std::size_t max_queue = 10;
   constexpr std::size_t global_capacity = 30 * 1024;
   constexpr std::size_t packet_size = std::size(package);
+  constexpr std::size_t IDLE_TIME_MAX = 5;
+
+  inline std::uint64_t now()noexcept{
+    using clock = std::chrono::steady_clock;
+    return std::chrono::duration_cast<std::chrono::seconds>(clock::now().time_since_epoch()).count();
+  }
+
 }
